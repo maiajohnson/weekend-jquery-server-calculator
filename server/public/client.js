f08980ca76440446c596166d3acb73a1615086bb
Maia Johnson <maiaj1306@gmail.com>
2022-10-05 19:57:16 -0500

    created onInput POST function and doMath function

--- server/public/client.js
+++ server/public/client.js
@@ -3,5 +3,32 @@ $(document).ready(onReady);
 function onReady() {
     console.log('in jquery');
 
+    $('#equals').on('click', onInput);
+}
+
+function onInput(evt) {
+    evt.preventDefault();
+
+    let inputValues = {
+        first: $('#num1').val(),
+        second: $('#num2').val(),
+    };
+
+    console.log('in onCalculate', inputValues);
+
+    $.ajax({
+        url: '/input',
+        method: 'POST',
+        data: inputValues
+    })
+        .then(response => {
+            console.log('POST inputValues', response);
+        })
+
+        .catch((err) => {
+            console.log('POST inputValues error', err);
+        })
     
+    $('#num1').val('');
+    $('#num2').val('');
 }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
+let equations = [];
 
 console.log('in server')
 
@@ -12,3 +13,32 @@ app.use(express.static('server/public'));
 app.listen(PORT, () => {
     console.log('server is running on port', PORT);
 });
+
+app.post('/input', (req, res) => {
+    console.log('inputvalues', req.body);
+
+    equations.push(req.body);
+    console.log('the values are now:', equations);
+
+    res.sendStatus(201);
+});
+
+function doMath() {
+    let result;
+
+    if ($('#add').on('click')) {
+        result = first + second;
+        return result;
+    } else if ($('#subtract').on('click')) {
+        result = first - second;
+        return result;
+    } else if ($('#multiply').on('click')) {
+        result = first * second;
+        return result;
+    } else if ($('#divide').on('click')) {
+        result = first / second;
+        return result;
+    }
+}
+
+app.get
